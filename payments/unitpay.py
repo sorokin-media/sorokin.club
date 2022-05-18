4441c8fd9c1079d0e7f7153ba782b06643a5a5d2
Mikhail <glader.ru@gmail.com>
2022-05-18 11:26:52 +0400

    Unitpay payments

---
+++ payments/unitpay.py
@@ -0,0 +1,86 @@
+import hashlib
+import json
+import logging
+from base64 import b64encode
+from dataclasses import dataclass
+from urllib.parse import urlencode
+from uuid import uuid4
+
+from django.conf import settings
+
+from users.models.user import User
+
+log = logging.getLogger(__name__)
+
+
+@dataclass
+class Invoice:
+    id: str
+    url: str
+
+
+class UnitpayService:
+    @classmethod
+    def create_payment(cls, product: dict, user: User) -> Invoice:
+        order_id = uuid4().hex
+
+        cash = [{
+            "name": "Сорокин.Клуб",
+            "count": 1,
+            "price": product["amount"],
+            "type": "commodity",
+        }]
+
+        params = {
+            "sum": str(product["amount"]),
+            "account": order_id,
+            "desc": "Сорокин.Клуб",
+            "currency": "RUB",
+            "backUrl": settings.APP_HOST,
+            "subscription": str(int(product["recurrent"])),
+            "customerEmail": user.email,
+            "cashItems": b64encode(json.dumps(cash).encode()),
+        }
+
+        params["signature"] = cls.make_signature(params)
+
+        url = "https://unitpay.ru/pay/" + settings.UNITPAY_PUBLIC_KEY + "?" + urlencode(params)
+
+        invoice = Invoice(
+            id=order_id,
+            url=url,
+        )
+        log.info("Created %s", invoice)
+
+        return invoice
+
+    @classmethod
+    def make_signature(cls, payload) -> str:
+        fields = ("account", "currency", "desc", "sum")
+        string = "{up}".join([payload[field] for field in fields] + [settings.UNITPAY_SECRET_KEY])
+        signature = hashlib.sha256(string.encode()).hexdigest()
+        return signature
+
+    @classmethod
+    def verify_webhook(cls, request) -> bool:
+        log.info("Verify request")
+
+        method = None
+        signature = None
+        params = dict()
+
+        for k, v in request.GET.items():
+            if k == "method":
+                method = v
+
+            elif k == "params[signature]":
+                signature = v
+
+            else:
+                k = k.replace("params[", "").replace("]", "")
+                params[k] = v
+
+        string = "{up}".join([method] + [params[k] for k in sorted(params.keys())] + [settings.UNITPAY_SECRET_KEY])
+        counted_signature = hashlib.sha256(string.encode()).hexdigest()
+
+        return signature == counted_signature
