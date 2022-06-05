0f402535970291147c7766202e37788ac58be56d
raskrutka89 <raskrutka89@gmail.com>
2022-06-05 10:47:46 +0400

    add command moderation check cron

--- comments/management/commands/check_new_comments.py
+++ comments/management/commands/check_new_comments.py
@@ -3,17 +3,17 @@ from datetime import datetime, timedelta
 from django.core.management import BaseCommand
 
 from comments.models import Comment
-from notifications.telegram.common import ADMIN_CHAT, send_telegram_message
+from notifications.telegram.common import ADMIN_CHAT, send_telegram_message, render_html_message
 
 
 class Command(BaseCommand):
     help = "Check new comment"
 
     def handle(self, *args, **options):
-        delta = datetime.now() - timedelta(minutes=1)
+        delta = datetime.now() - timedelta(hours=6)
         comments_query = Comment.objects.filter(created_at__gte=delta)
         if not comments_query:
             send_telegram_message(
                 chat=ADMIN_CHAT,
-                text="Уже 6 часов не было ни одного нового комментария",
+                text=render_html_message("moderator_no_new_comment.html"),
             )
