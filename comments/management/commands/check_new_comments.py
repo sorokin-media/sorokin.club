894c804db8a14d082b62f2c2cb98ce95d25c6216
raskrutka89 <raskrutka89@gmail.com>
2022-06-05 10:37:55 +0400

    add command moderation check cron

--- comments/management/commands/check_new_comments.py
+++ comments/management/commands/check_new_comments.py
@@ -1,23 +1,19 @@
 from datetime import datetime, timedelta
 
 from django.core.management import BaseCommand
-from django.db import connection
 
 from comments.models import Comment
-from notifications.telegram.common import ADMIN_CHAT, send_telegram_message, render_html_message, Chat
+from notifications.telegram.common import ADMIN_CHAT, send_telegram_message
 
 
 class Command(BaseCommand):
     help = "Check new comment"
 
     def handle(self, *args, **options):
-        print('Зашел 2')
         delta = datetime.now() - timedelta(minutes=1)
         comments_query = Comment.objects.filter(created_at__gte=delta)
         if not comments_query:
-            print('Зашел comments_query')
             send_telegram_message(
                 chat=ADMIN_CHAT,
                 text="Уже 6 часов не было ни одного нового комментария",
             )
-        print('Конец')
--- posts/management/commands/check_new_post.py
+++ posts/management/commands/check_new_post.py
@@ -1,9 +1,7 @@
 from datetime import datetime, timedelta
 
 from django.core.management import BaseCommand
-from django.db import connection
 
-from club.settings import POST_HOTNESS_PERIOD
 from posts.models.post import Post
 from notifications.telegram.common import ADMIN_CHAT, send_telegram_message, render_html_message, Chat
 
@@ -20,10 +18,9 @@ class Command(BaseCommand):
                 text=render_html_message("moderator_no_new_post.html"),
             )
         delta_intro = datetime.now() - timedelta(minutes=1)
-        posts_query = Post.objects.filter(created_at__gte=delta).filter(type='intro')
+        posts_query = Post.objects.filter(created_at__gte=delta_intro).filter(type='intro')
         if not posts_query:
             send_telegram_message(
                 chat=ADMIN_CHAT,
                 text=render_html_message("moderator_no_new_intro.html"),
             )
-
