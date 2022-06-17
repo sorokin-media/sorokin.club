cfb137c77c7d5736594c0cdab040bd517665c066
raskrutka89 <raskrutka89@gmail.com>
2022-06-17 17:38:40 +0400

    add column unitpay

--- users/migrations/0025_add_unitpay_column.py
+++ users/migrations/0025_add_unitpay_column.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
-            model_name='users',
+            model_name='user',
             name='unitpay_id',
             field=models.CharField(max_length=128, null=True),
         ),
