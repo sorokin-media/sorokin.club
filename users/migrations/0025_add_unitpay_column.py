3a53e319d1e0b99fa6860bd5463b9cce9e1de84a
raskrutka89 <raskrutka89@gmail.com>
2022-06-17 17:32:16 +0400

    add column unitpay

--- users/migrations/0025_add_unitpay_column.py
+++ users/migrations/0025_add_unitpay_column.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0024_auto_20210512_1053'),
+        ('users', '0024_muted_comment'),
     ]
 
     operations = [
