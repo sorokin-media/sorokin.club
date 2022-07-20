5cbf9d804e80237b23ff746fc05d05d93aa1b75b
raskrutka89 <raskrutka89@gmail.com>
2022-07-20 19:44:49 +0400

    fix plans first checked

--- auth/management/commands/test_command.py
+++ auth/management/commands/test_command.py
@@ -7,33 +7,47 @@ class Command(BaseCommand):
     help = "Fetches expiring accounts and tries to renew the subscription"
 
     def handle(self, *args, **options):
-        subscription = Subscription(name="Default", default=True)
+        # subscription = Subscription(name="Default", default=True)
+        # subscription.save()
+        # subscription_plan = SubscriptionPlan(subscription_id=subscription.id, name='На 1 год дефолт', amount=1200,
+        #                                      description='1 год членства в Клубе', code='club12', timedelta=365,
+        #                                      package_name='На 1 год', package_image='🤘', package_price=1200)
+        # subscription_plan.save()
+        # subscription_plan = SubscriptionPlan(subscription_id=subscription.id, name='На 2 года дефолт', amount=2400,
+        #                                      description='2 года членства в Клубе', code='club24', timedelta=730,
+        #                                      package_name='На 2 года', package_image='😎', package_price=2400)
+        # subscription_plan.save()
+        # subscription_plan = SubscriptionPlan(subscription_id=subscription.id, name='На 10 лет дефолт', amount=9000,
+        #                                      description='10 год членства в Клубе', code='club120', timedelta=3650,
+        #                                      package_name='На 10 лет', package_image='🚀', package_price=9000)
+        # subscription_plan.save()
+        #
+        # subscription_invite = Subscription(name="Invite", default=False)
+        # subscription_invite.save()
+        #
+        # subscription_plan = SubscriptionPlan(subscription_id=subscription_invite.id, name='За друга', amount=1200,
+        #                                      description='Пригласить друга в Клуб', code='club1_invite', timedelta=365,
+        #                                      package_name='Позвать в Клуб', package_image='🚀', package_price=1200)
+        # subscription_plan.save()
+        #
+        # subscription_test = Subscription(name="Test", default=False)
+        # subscription_test.save()
+        #
+        # subscription_plan = SubscriptionPlan(subscription_id=subscription_test.id, name='тест', amount=12,
+        #                                      description='Тест дляя меня', code='club12_test', timedelta=30,
+        #                                      package_name='Ntcn gjlgbcrf', package_image='🚀', package_price=12)
+        # subscription_plan.save()
+        subscription = Subscription(name="DefaultNew", default=False)
         subscription.save()
-        subscription_plan = SubscriptionPlan(subscription_id=subscription.id, name='На 1 год дефолт', amount=1200,
-                                             description='1 год членства в Клубе', code='club12', timedelta=365,
-                                             package_name='На 1 год', package_image='🤘', package_price=1200)
+        subscription_plan = SubscriptionPlan(subscription_id=subscription.id, name='На 1 месяц дефолт', amount=190,
+                                             description='1 месяц членства в Клубе', code='club1m', timedelta=30,
+                                             package_name='На 1 месяц', package_image='🤘', package_price=190)
         subscription_plan.save()
-        subscription_plan = SubscriptionPlan(subscription_id=subscription.id, name='На 2 года дефолт', amount=2400,
-                                             description='2 года членства в Клубе', code='club24', timedelta=730,
-                                             package_name='На 2 года', package_image='😎', package_price=2400)
+        subscription_plan = SubscriptionPlan(subscription_id=subscription.id, name='На 6 месяцев дефолт', amount=990,
+                                             description='6 месяцев членства в Клубе', code='club6m', timedelta=180,
+                                             package_name='На 6 месяцев', package_image='😎', package_price=990)
         subscription_plan.save()
-        subscription_plan = SubscriptionPlan(subscription_id=subscription.id, name='На 10 лет дефолт', amount=9000,
-                                             description='10 год членства в Клубе', code='club120', timedelta=3650,
-                                             package_name='На 10 лет', package_image='🚀', package_price=9000)
-        subscription_plan.save()
-
-        subscription_invite = Subscription(name="Invite", default=False)
-        subscription_invite.save()
-
-        subscription_plan = SubscriptionPlan(subscription_id=subscription_invite.id, name='За друга', amount=1200,
-                                             description='Пригласить друга в Клуб', code='club1_invite', timedelta=365,
-                                             package_name='Позвать в Клуб', package_image='🚀', package_price=1200)
-        subscription_plan.save()
-
-        subscription_test = Subscription(name="Test", default=False)
-        subscription_test.save()
-
-        subscription_plan = SubscriptionPlan(subscription_id=subscription_test.id, name='тест', amount=12,
-                                             description='Тест дляя меня', code='club12_test', timedelta=30,
-                                             package_name='Ntcn gjlgbcrf', package_image='🚀', package_price=12)
+        subscription_plan = SubscriptionPlan(subscription_id=subscription.id, name='На 1 год дефолт', amount=1790,
+                                             description='1 год членства в Клубе', code='club12m', timedelta=365,
+                                             package_name='На 1 год', package_image='🤘', package_price=1790)
         subscription_plan.save()
