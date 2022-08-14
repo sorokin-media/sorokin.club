1b0ad486e8cb7beb2ea2fa1ee35819342d84c4a8
raskrutka89 <raskrutka89@gmail.com>
2022-08-14 10:41:04 +0400

    запись кук, отправка в телегу при реге

--- auth/management/commands/test_command.py
+++ auth/management/commands/test_command.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     help = "Fetches expiring accounts and tries to renew the subscription"
 
     def handle(self, *args, **options):
-        user = User.objects.filter(id='fadac4b3-152c-4181-8e5f-2269a2df9d95').last()
+        user = User.objects.filter(id='bdde174c-c487-48f6-b684-b400d469d0d8').last()
         pay = Payment.objects.filter(user_id=user.id, status='success').last()
         cookie_auth = '13.08.2022, 12:08:17,last_page=_post_16_/13.08.2022, 12:08:35,last_page=_post_16_/13.08.2022, 12:09:49,last_page=_post_16_/14.08.2022, 10:07:03,last_page=_post_16_/'
         text_send = user.email + ' ' + str(pay.amount) + "\n" + cookie_auth.replace('/', "\n")
