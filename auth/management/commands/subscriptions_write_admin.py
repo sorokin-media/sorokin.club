ff23331b1427489e95fcd9c5328533ece9d41263
raskrutka89 <raskrutka89@gmail.com>
2022-10-20 10:24:09 +0400

    fix message admin user payment

---
+++ auth/management/commands/subscriptions_write_admin.py
@@ -0,0 +1,22 @@
+import json
+from datetime import datetime, timedelta
+from django.core.management import BaseCommand
+from users.models.user import User
+from notifications.telegram.users import cancel_subscribe_admin
+
+
+class Command(BaseCommand):
+    help = "Fetches expiring accounts and tries to renew the subscription"
+
+    def handle(self, *args, **options):
+        expiring_users = User.objects.filter(membership_expires_at__gte=datetime.utcnow() + timedelta(seconds=-330),
+                                             membership_expires_at__lte=datetime.utcnow(),
+                                             moderation_status='approved',
+                                             deleted_at__isnull=True)
+        for user in expiring_users:
+            self.stdout.write(f"Checking user: {user.slug}")
+            self.cancelSubUser(user)
+        self.stdout.write("Done 🥙")
+
+    def cancelSubUser(self, user):
+        cancel_subscribe_admin(user)
