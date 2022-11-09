f1243792031766519900ecabaaa793c450cf9f6f
raskrutka89 <raskrutka89@gmail.com>
2022-11-09 16:33:38 +0400

    fix publish post type

--- posts/renderers.py
+++ posts/renderers.py
@@ -79,6 +79,16 @@ def render_post(request, post, context=None):
         return redirect("show_post", post.type, post.slug)
     if post.type == 'idea':
         return render(request, "posts/show/idea.html", context)
+    if post.type == 'event':
+        return render(request, "posts/show/event.html", context)
+    if post.type == 'question':
+        return render(request, "posts/show/question.html", context)
+    if post.type == 'link':
+        return render(request, "posts/show/link.html", context)
+    if post.type == 'battle':
+        return render(request, "posts/show/battle.html", context)
+    if post.type == 'post':
+        return render(request, "posts/show/post.html", context)
     try:
         return render(request, f"posts/show/{post.type}.html", context)
     except TemplateDoesNotExist:
