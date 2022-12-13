7b7a6a13ad83ffc612491b983346e6783ee15011
Your Name <romashov.dmitry.py@gmail.com>
2022-12-14 00:21:33 +0400

    test on more

--- buddy/management/commands/check_posts_for_buddy.py
+++ buddy/management/commands/check_posts_for_buddy.py
@@ -28,31 +28,19 @@ def send_to_buddy_group(bot, hours, hours_words, slug, intro_id, lattest_action)
         # if user is authorized in telegram bot
         if User.objects.filter(id=author_id).first().telegram_id:
             # if intro is approved
-            if Post.objects.filter(id=intro_id).first().is_approved_by_moderator is True:
-                utc_comment_time = time_zone.localize(lattest_action)
-                utc_time_to_comment = time_zone.localize(datetime.utcnow()-timedelta(minutes=1))
-                if utc_comment_time < utc_time_to_comment:
-                    post = Post.objects.filter(id=intro_id).first()
-                    if post.time_task_sended:
-                        time_to_send_tusk = time_zone.localize(datetime.utcnow()-timedelta(hours=12))
-                        time_tusk_was_sended = time_zone.localize(post.time_task_sended)
-                        if time_tusk_was_sended < time_to_send_tusk:
-                            post.set_time_for_tusk()                
-                            bot.send_message(chat_id=-720612660,
-                                            parse_mode=ParseMode.HTML,
-                                            text=f'Пользователю никто не написал по итогу предыдущего задания! \n'
-                                                'Давайте расспросим его!\n'
-                                                f'<a href=\"{settings.APP_HOST}/intro/{slug}\">Ссылка '
-                                                'на интро</a>',
-                                            reply_markup=telegram.InlineKeyboardMarkup([
-                                                *[
-                                                [telegram.InlineKeyboardButton("Я задам! 💪",
-                                                callback_data=f'buddy_get_intro {intro_id}')]]]))
-                    else:
-                        post.set_time_for_tusk()
+#            if Post.objects.filter(id=intro_id).first().is_approved_by_moderator is True:
+            utc_comment_time = time_zone.localize(lattest_action)
+            utc_time_to_comment = time_zone.localize(datetime.utcnow()-timedelta(minutes=1))
+            if utc_comment_time < utc_time_to_comment:
+                post = Post.objects.filter(id=intro_id).first()
+                if post.time_task_sended:
+                    time_to_send_tusk = time_zone.localize(datetime.utcnow()-timedelta(hours=12))
+                    time_tusk_was_sended = time_zone.localize(post.time_task_sended)
+                    if time_tusk_was_sended < time_to_send_tusk:
+                        post.set_time_for_tusk()                
                         bot.send_message(chat_id=-720612660,
                                         parse_mode=ParseMode.HTML,
-                                        text=f'{hours_words} без комментариев 😮\n'
+                                        text=f'Пользователю никто не написал по итогу предыдущего задания! \n'
                                             'Давайте расспросим его!\n'
                                             f'<a href=\"{settings.APP_HOST}/intro/{slug}\">Ссылка '
                                             'на интро</a>',
@@ -60,6 +48,18 @@ def send_to_buddy_group(bot, hours, hours_words, slug, intro_id, lattest_action)
                                             *[
                                             [telegram.InlineKeyboardButton("Я задам! 💪",
                                             callback_data=f'buddy_get_intro {intro_id}')]]]))
+                else:
+                    post.set_time_for_tusk()
+                    bot.send_message(chat_id=-720612660,
+                                    parse_mode=ParseMode.HTML,
+                                    text=f'{hours_words} без комментариев 😮\n'
+                                        'Давайте расспросим его!\n'
+                                        f'<a href=\"{settings.APP_HOST}/intro/{slug}\">Ссылка '
+                                        'на интро</a>',
+                                    reply_markup=telegram.InlineKeyboardMarkup([
+                                        *[
+                                        [telegram.InlineKeyboardButton("Я задам! 💪",
+                                        callback_data=f'buddy_get_intro {intro_id}')]]]))
 
 class Command(BaseCommand):
     '''
