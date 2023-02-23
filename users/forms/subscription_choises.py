807db5292ab15e7e0531afa7732f62cf321ce690
KirillUshakov <cool.ushackov2016@yandex.ru>
2023-02-23 04:26:29 +0300

    Render checkboxes on subscription_settings page

--- users/forms/subscription_choises.py
+++ users/forms/subscription_choises.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
 from users.models.subscription import User
+from django.forms import CheckboxInput
 
-class ChooseSubscription(ModelForm): 
+class ChooseSubscription(ModelForm):
     class Meta:
         model = User
         fields = ('tg_yesterday_best_posts', 'tg_weekly_best_posts', 'daily_email_digest', 'weekly_email_digest')
+        widgets = {'tg_yesterday_best_posts': CheckboxInput(),
+        'tg_weekly_best_posts': CheckboxInput(),
+        'daily_email_digest': CheckboxInput(),
+        'weekly_email_digest': CheckboxInput()}
