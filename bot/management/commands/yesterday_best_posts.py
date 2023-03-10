4b6b3a5287a6750e03ec038fab2d99aa0bf2205c
romashovdmitry <73075611+romashovdmitry@users.noreply.github.com>
2023-03-10 15:56:04 +0400

    delete import of model "Import". bug fix.

--- bot/management/commands/yesterday_best_posts.py
+++ bot/management/commands/yesterday_best_posts.py
@@ -5,7 +5,7 @@ from club import settings
 from posts.templatetags.text_filters import rupluralize
 
 from posts.models.post import Post, PostExceptions
-from users.models.user import User, Delete
+from users.models.user import User
 from users.models.mute import Muted
 from comments.models import Comment
 
