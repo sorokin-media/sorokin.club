9afc4baa7dfc181644fa23a45f85674e24f27e60
raskrutka89 <raskrutka89@gmail.com>
2023-05-13 10:34:14 +0400

    old plans for users

--- users/views/settings.py
+++ users/views/settings.py
@@ -138,14 +138,14 @@ def edit_payments(request, user_slug):
         if plan_query:
             plans = SubscriptionPlan.objects.filter(subscription_id=plan_query.subscription_id).order_by("created_at")
         else:
-            if request.me.email == 'petrblagov@gmail.com' or request.me.email == 'rupronin@gmail.com' or request.me.email == 'raskrutka89@gmail.com':
+            if request.me.email == 'petrblagov@gmail.com' or request.me.email == 'rupronin@gmail.com':
                 # Включаем им старый тариф
                 plans = SubscriptionPlan.objects.filter(subscription_id='464b5b07-b44e-4d91-8529-9aa6f3fa1246').order_by("created_at")
             else:
                 plan_subcription = Subscription.objects.filter(default=True).last()
                 plans = SubscriptionPlan.objects.filter(subscription_id=plan_subcription.id).order_by("created_at")
     else:
-        if request.me.email == 'petrblagov@gmail.com' or request.me.email == 'rupronin@gmail.com' or request.me.email == 'raskrutka89@gmail.com':
+        if request.me.email == 'petrblagov@gmail.com' or request.me.email == 'rupronin@gmail.com':
             # Включаем им старый тариф
             plans = SubscriptionPlan.objects.filter(subscription_id='464b5b07-b44e-4d91-8529-9aa6f3fa1246').order_by("created_at")
         else:
