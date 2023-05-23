29264ff89bb9fdc93c5013da3364154deff25b93
raskrutka89 <raskrutka89@gmail.com>
2023-05-23 20:14:52 +0400

    plans

--- users/views/settings.py
+++ users/views/settings.py
@@ -85,7 +85,7 @@ def edit_notifications(request, user_slug):
     if user.id != request.me.id and not request.me.is_moderator:
         raise Http404()
 
-    if request.method == "POST":        
+    if request.method == "POST":
         form = sub_form(request.POST)
         if form.is_valid():
             user = User.objects.get(slug=user_slug)
@@ -141,8 +141,14 @@ def edit_payments(request, user_slug):
             plan_subcription = Subscription.objects.filter(default=True).last()
             plans = SubscriptionPlan.objects.filter(subscription_id=plan_subcription.id).order_by("created_at")
     else:
-        plan_subcription = Subscription.objects.filter(default=True).last()
-        plans = SubscriptionPlan.objects.filter(subscription_id=plan_subcription.id).order_by("created_at")
+        if request.me.email == 'petrblagov@gmail.com' or request.me.email == 'rupronin@gmail.com':
+            # Включаем им старый тариф
+            plans = SubscriptionPlan.objects.filter(subscription_id='464b5b07-b44e-4d91-8529-9aa6f3fa1246').order_by("created_at")
+            if request.me.email == 'raskrutka89@gmail.com':
+                plans = SubscriptionPlan.objects.filter(subscription_id='ea5d1894-fb02-4266-8083-23af90d393b0').order_by("created_at")
+        else:
+            plan_subcription = Subscription.objects.filter(default=True).last()
+            plans = SubscriptionPlan.objects.filter(subscription_id=plan_subcription.id).order_by("created_at")
 
     # if user.telegram_id == '204349098':
     #     plan_subcription = Subscription.objects.filter(name='Test').last()
