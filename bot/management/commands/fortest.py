36014843254ba420d38b0ecd5d92392e50025451
Your Name <romashov.dmitry.py@gmail.com>
2023-05-29 17:17:07 +0400

    test of picture

--- bot/management/commands/fortest.py
+++ bot/management/commands/fortest.py
@@ -20,13 +20,29 @@ from bot.sending_message import TelegramCustomMessage
 
 from club.settings import TG_DEVELOPER_DMITRY
 
+import telegram
+from telegram import Update, ParseMode
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import CallbackContext
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        bot = telegram.Bot(token=settings.TELEGRAM_TOKEN)
+
         try:
 
-            print(TG_DEVELOPER_DMITRY)
+            u = User.objects.get(slug='Anna_Golubova')
+            text = "Картинка Нюте! (:"
+            chat_id = u.telegram_id
+            photo = 'https://sorokin.club/static/images/random_coffee.jpg'
+
+            bot.send_photo(
+                chat_id=chat_id,
+                photo=photo,
+                caption=text,
+                parse_mode=ParseMode.HTML)
 
         except Exception:
 
