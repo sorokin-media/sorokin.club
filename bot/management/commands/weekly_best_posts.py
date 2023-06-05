b36c89823be20760c6e8745976917de4ce6ce26e
Your Name <romashov.dmitry.py@gmail.com>
2023-06-05 12:15:59 +0400

    test done, everything ok.

--- bot/management/commands/weekly_best_posts.py
+++ bot/management/commands/weekly_best_posts.py
@@ -164,7 +164,7 @@ def compile_message_helper(bot, users_for_yesterday_digest, dict_list, header_of
                 string_for_bot = header_of_message + string_for_bot + optional
             else:
                 string_for_bot = header_of_message + string_for_bot
-            if user.slug not in ['AlekseiPodkletnov', 'den-is', 'Lisa', 'nabiullin', 'hichnii', 'rav'] and user.slug not in ['vika', 'skorpion28', 'sesevor'] and user.slug in ['romashovdmitryo']:
+            if user.slug not in ['AlekseiPodkletnov', 'den-is', 'Lisa', 'nabiullin', 'hichnii', 'rav'] and user.slug not in ['vika', 'skorpion28', 'sesevor']:
                 if 'посты' in header_of_message:
                     custom_message = TelegramCustomMessage(
                         etc=author,
