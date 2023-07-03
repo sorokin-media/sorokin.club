f962f57d1aa32864f57cbb3785d77262c65f9ee1
Your Name <romashov.dmitry.py@gmail.com>
2023-07-03 12:47:21 +0400

    catch bug

--- bot/management/commands/weekly_best_posts.py
+++ bot/management/commands/weekly_best_posts.py
@@ -191,6 +191,7 @@ def compile_message_helper(bot, users_for_weekly_digest, dict_list, header_of_me
             else:
                 string_for_bot = header_of_message + string_for_bot
             if user.slug not in ['AlekseiPodkletnov', 'den-is', 'Lisa', 'nabiullin', 'hichnii', 'rav'] and user.slug not in ['vika', 'skorpion28', 'sesevor']:
+                MessageToDmitry(data=f'{header_of_message}').send_message()
                 if 'лучшие посты' in header_of_message:
                     custom_message = TelegramCustomMessage(
                         etc=author,
