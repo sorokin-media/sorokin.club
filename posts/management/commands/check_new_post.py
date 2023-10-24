7db8a4de37938be39e83cd3665211c9a60b6e578
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2023-10-24 22:56:20 +0400

    убрал проверку на наличие новых постов за 18 часов

--- posts/management/commands/check_new_post.py
+++ posts/management/commands/check_new_post.py
@@ -11,13 +11,13 @@ class Command(BaseCommand):
     help = "Check new post"
 
     def handle(self, *args, **options):
-        delta = datetime.now() - timedelta(hours=18)
-        posts_query = Post.objects.filter(published_at__gte=delta, is_approved_by_moderator=True).filter(Q(type=Post.TYPE_POST) | Q(type=Post.TYPE_LINK) | Q(type=Post.TYPE_QUESTION) | Q(type=Post.TYPE_IDEA) | Q(type=Post.TYPE_PROJECT) | Q(type=Post.TYPE_EVENT) | Q(type=Post.TYPE_REFERRAL) | Q(type=Post.TYPE_BATTLE) | Q(type=Post.TYPE_GUIDE) | Q(type=Post.TYPE_THREAD))
-        if not posts_query:
-            send_telegram_message(
-                chat=ADMIN_CHAT,
-                text=render_html_message("moderator_no_new_post.html"),
-            )
+#        delta = datetime.now() - timedelta(hours=18)
+#        posts_query = Post.objects.filter(published_at__gte=delta, is_approved_by_moderator=True).filter(Q(type=Post.TYPE_POST) | Q(type=Post.TYPE_LINK) | Q(type=Post.TYPE_QUESTION) | Q(type=Post.TYPE_IDEA) | Q(type=Post.TYPE_PROJECT) | Q(type=Post.TYPE_EVENT) | Q(type=Post.TYPE_REFERRAL) | Q(type=Post.TYPE_BATTLE) | Q(type=Post.TYPE_GUIDE) | Q(type=Post.TYPE_THREAD))
+#        if not posts_query:
+#            send_telegram_message(
+#                chat=ADMIN_CHAT,
+#                text=render_html_message("moderator_no_new_post.html"),
+#            )
         delta = datetime.now() - timedelta(hours=24)
         posts_query = Post.objects.filter(announce_at__gte=delta).exclude(announce_at__isnull=True)
         if not posts_query:
