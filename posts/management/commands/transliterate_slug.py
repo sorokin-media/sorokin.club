77e78f8d3b57398155bd3894de7b9cec651a0007
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2023-11-09 22:07:44 +0400

    update transliterating slugs

--- posts/management/commands/transliterate_slug.py
+++ posts/management/commands/transliterate_slug.py
@@ -31,8 +31,11 @@ class Command(BaseCommand):
                     post_title = post_title.strip()
                     # remove all except letters, numbers and spaces
                     post_title = re.sub("[^A-Za-z\d\s]", "", post_title)
-                    post_title = post_title.replace("  ", "").replace(" ", "-").replace("--", "-")
                     post_slug = post.slug + f"-{post_title}"
+                    post_title = post_title.replace(" ", "-").replace("--", "-")
+                    while post_slug[-1] == '-':
+                        post_slug = post_slug[:-1]
+
 #                    post.slug = post_slug
 #                    post.save()
                     output_file.write(f'{post_slug}\n')
