a101343764b5d92f212c4be2db3216bb6a0a8ca1
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2023-11-14 21:22:20 +0400

    update transliteration

--- posts/management/commands/transliterate_slug.py
+++ posts/management/commands/transliterate_slug.py
@@ -19,9 +19,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         ''' get posts titles, translitarate them all '''
         posts = Post.objects.filter(type='post').all()
-
-        output_file_path = "new_slugs.txt"
-        with open(output_file_path, "w") as output_file:
+        # for test on prod
+        output_file_path = 'transliterated_slugs.txt'
+        with open(output_file_path, 'w', encoding='utf-8') as output_file:
 
             for post in posts:
                 
@@ -31,12 +31,16 @@ class Command(BaseCommand):
                     post_title = post_title.strip()
                     # remove all except letters, numbers and spaces
                     post_title = re.sub("[^A-Za-z\d\s]", "", post_title)
-                    post_title = post_title.replace("  ", "").replace(" ", "-").replace("--", "-")
                     post_slug = post.slug + f"-{post_title}"
+                    self.stdout.write(post_slug)
+                    post_slug = post_slug.replace(" ", "-").replace("--", "-")
+                    while post_slug[-1] == '-':
+                        post_slug = post_slug[:-1]
+
                     post.slug = post_slug
                     post.save()
-                    output_file.write(post.slug + "\n")  
-                    self.stdout.write(post.slug)
+                    output_file.write(f'{post_slug}\n')
+                    self.stdout.write(post_slug)
 
                 except Exception as ex:
 
