3942f7f737ee158ee242259faaf1b33f0a641a4a
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2023-11-21 17:01:02 +0400

    fix one more syntax

--- bot_payment_checker/management/commands/start_bot.py
+++ bot_payment_checker/management/commands/start_bot.py
@@ -70,9 +70,8 @@ class Command(BaseCommand):
                 User.objects.filter(telegram_id=telegram_id).exclude(slug__in=exception_list).first()
                 for telegram_id in chat_users
                 # TO FIX: bad code. Так сделано из-за наличия трёх тг на одном юзере, см. exception_list
-                if User.objects.filter(telegram_id=telegram_id).exclude(slug_in=exception_list).first() is not None
+                if User.objects.filter(telegram_id=telegram_id).exclude(slug__in=exception_list).first() is not None
             ]
-
         except Exception as ex:
             log.error(f"Exception in pyament_bot: {ex}")
             MessageToDmitry(data=f"error in club_users. Ex: {ex}").send_message()
@@ -89,3 +88,4 @@ class Command(BaseCommand):
             log.error(f"Exception in pyament_bot: {ex}")
             MessageToDmitry(data=f"error in expired_user_or_not. Ex: {ex}").send_message()
 
+        MessageToDmitry(data="finished").send_message()
