d6e8f89deb35e442fecd41e36ab84c0d0eb7cd61
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2024-01-02 13:02:34 +0400

    test money group

--- bot/management/commands/fortest.py
+++ bot/management/commands/fortest.py
@@ -6,43 +6,32 @@ from django.core.management import BaseCommand
 from users.models.user import User
 from users.models.random_coffee import RandomCoffee
 
-# imports for getting config data
-from club import settings
-
 # Telegram imports
-import telegram
-from telegram import Update, ParseMode
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CallbackContext
-
-# import custom class for sending message
-from bot.sending_message import TelegramCustomMessage
-
-from club.settings import TG_DEVELOPER_DMITRY
-
 import telegram
 from telegram import Update, ParseMode
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext
 
+# imports for getting config data
+from club import settings
+from notifications.telegram.common import TELEGRAM_CLUB_MONEY_GROUP
+from club.settings import TG_DEVELOPER_DMITRY
+
+# import custom class for sending message
+from bot.sending_message import TelegramCustomMessage
+from notifications.telegram.common import send_telegram_message
+
 class Command(BaseCommand):
-
+    """ Django commands """
     def handle(self, *args, **options):
-
-        bot = telegram.Bot(token=settings.TELEGRAM_TOKEN)
+        """ testing bot """
 
         try:
 
-            u = User.objects.get(slug='Anna_Golubova')
-            text = "Картинка Нюте! (:"
-            chat_id = u.telegram_id
-            photo = 'https://sorokin.club/static/images/random_coffee.jpg'
-
-            bot.send_photo(
-                chat_id=chat_id,
-                photo=photo,
-                caption=text,
-                parse_mode=ParseMode.HTML)
+            send_telegram_message(
+                chat=TELEGRAM_CLUB_MONEY_GROUP,
+                text=f"#тестработыбота",
+            )
 
         except Exception:
 
