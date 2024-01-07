7c6d4a88ca360f4ec4e43c09af7556ea07cc37bb
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2024-01-07 11:14:42 +0400

    catch bug

---
+++ bot/management/commands/random_coffee_test.py
@@ -0,0 +1,31 @@
+# import Django packages
+from django.core.management import BaseCommand
+
+# import Models
+from users.models.random_coffee import RandomCoffee
+
+# import custom package for sending message
+from bot.sending_message import TelegramCustomMessage, MessageToDmitry
+
+
+text_for_message = 'Привет! Напоминаю, что ты участвуешь в Random Coffee!\n' \
+    'Если на этой неделе ты не хочешь ни с кем знакомиться, нажми кнопку👇 Ждем твоего ответа до 19-00 мск.'
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        coffee_users = RandomCoffee.objects.filter(random_coffee_is=True).all()
+
+        for coffee_user in coffee_users:
+
+            if not coffee_user.user.membership_expires_is():
+
+                test_message = (
+                    coffee_user.user.slug + ": " + coffee_user.user + "\n"
+                    "random_coffee_last_partner_id: " + coffee_user.random_coffee_last_partner_id  + '\n'
+                    "coffee_user.user.membership_expires_is(): " + coffee_user.user.membership_expires_is() + "\n"
+                )
+                MessageToDmitry(data=test_message).send_message()
+
+        MessageToDmitry(data=Exception).send_message()
